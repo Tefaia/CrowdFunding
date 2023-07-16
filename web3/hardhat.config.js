18b71b6d9a14dbea1a017222a12e184366403962
Tefaia Edmondson <2010tefaia@gmail.com>
2023-07-16 09:42:15 -0700

    CrowdFunding initial commit

---
+++ web3/hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/base_goerli/9455dcebbb5c07b3f51f064bc5f897d5d6640b1ade6ab13390ab8066d98e9118',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
