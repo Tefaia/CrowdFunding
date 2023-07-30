e165e3b9bafe4cc45f2ba750b56db233efd7badf
Tefaia <2010tefaia@gmail.com>
2023-07-30 15:51:05 -0700

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import { Sidebar, Navbar } from './components';
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
 
